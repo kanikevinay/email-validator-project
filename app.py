7f9b0f2a2ef2ab7731f6f618f2023ab7d902a0f0
kanikevinay <kanikevinay709@gmail.com>
2026-05-28 13:10:14 +0530

    Initial commit

--- app.py
+++ app.py
@@ -8,3 +8,4 @@ import sys
 PROJECT_DIR = Path(__file__).resolve().parent / "email_verifier"
 sys.path.insert(0, str(PROJECT_DIR))
 runpy.run_path(str(PROJECT_DIR / "app.py"), run_name="__main__")
+  
